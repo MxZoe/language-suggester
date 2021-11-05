d6fbe6bc97ddabcaf6e9117c1c90769ef38dbd12
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-05 14:40:17 -0700

    add actual questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-//business logic
 
 //UI logic
 $(document).ready(function(){
   //function to make the next button show the next section and hide the previous
+  let counter = 0;
+  let tally = 0;
   $("#nextButton").click(function(event){
-    let counter = 0;
-    let tally = 0;
     let checkedValue =0;
     if(counter == 0){
       $("#questionOne").hide();
@@ -51,6 +50,7 @@ $(document).ready(function(){
     $("#restartButton").hide();
     counter = 0;
     tally = 0;
+    event.preventDefault();
   });
 
   //function to submit the form and return a language. Ranges for language selection are not even.
