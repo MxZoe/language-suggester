84c9a112aa303c97de5f3a5cd1d6527d2412ad52
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-04 17:08:01 -0700

    add code for submiting the form

--- js/scripts.js
+++ js/scripts.js
@@ -40,5 +40,27 @@ $(document).ready(function(){
     }
     event.preventDefault();
   });
-
+  $("form#formOne").submit(function(event){
+    checkedValue = parseInt($("input[name='question1']:checked").val());
+    tally += checkedValue;
+    if(tally <= 5){
+      $("#insertResult").append("Your language is Python!")
+    }
+    else if(tally > 5 && tally <= 10){
+      $("#insertResult").append("Your language is C#!")
+    }
+    else if(tally > 11 && tally <= 15){
+      $("#insertResult").append("Your language is Java!")
+    }
+    else if(tally > 15 && tally <=20){
+      $("#insertResult").append("Your language is Javascript!")
+    }
+    else{
+      $("#insertResult").append("Your language is unknown. Sorry! Something went wrong")
+    }
+    $("#submitButton").hide();
+    $("#questionFive").hide()
+    $("#result").show();
+    event.preventDefault();
+  });
 });
