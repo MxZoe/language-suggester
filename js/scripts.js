7c16faa264b8e2fe9cad25538059d16bf7c459cc
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-06 13:33:12 -0700

    added footer, still needs adjustment

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,32 @@
-
+//business logic
+let counter = 1;
+let tally = 0;
 //UI logic
 $(document).ready(function(){
-  //function to make the next button show the next section and hide the previous
-  let counter = 0;
-  let tally = 0;
   $("#nextButton").click(function(event){
     let checkedValue =0;
-    if(counter == 0){
+    if(counter == 1){
       $("#questionOne").hide();
       $("#questionTwo").show();
       checkedValue = parseInt($("input[name='question1']:checked").val());
       tally += checkedValue;
       counter++;
     }
-    else if(counter == 1){
+    else if(counter == 2){
       $("#questionTwo").hide();
       $("#questionThree").show();
       checkedValue = parseInt($("input[name='question2']:checked").val());
       tally += checkedValue;
       counter++;
     }
-    else if(counter == 2){
+    else if(counter == 3){
       $("#questionThree").hide();
       $("#questionFour").show();
       checkedValue = parseInt($("input[name='question3']:checked").val());
       tally += checkedValue;
       counter++;
     }
-    else if(counter == 3){
+    else if(counter == 4){
       $("#questionFour").hide();
       $("#nextButton").hide()
       $("#questionFive").show();
@@ -37,42 +36,31 @@ $(document).ready(function(){
       counter = 1;
     }
     else{
-      counter = 0;
+      counter = 1;
     }
     event.preventDefault();
   });
-
-  $("#restartButton").click(function(event){
-    $("#result").hide();
-    $("#questionOne").show();
-    $("#nextButton").show();
-    $("submitButton").hide();
-    $("#restartButton").hide();
-    counter = 0;
-    tally = 0;
-    event.preventDefault();
-  });
-
-  //function to submit the form and return a language. Ranges for language selection are not even.
   $("form#formOne").submit(function(event){
     checkedValue = parseInt($("input[name='question1']:checked").val());
     tally += checkedValue;
-    if(tally >= 5 && tally <= 10){
-      $("#insertResult").text("Your language is Python!")
+    if(tally <= 5){
+      $("#insertResult").append("Your language is Python!")
     }
-    else if(tally >= 11 && tally <= 15){
-      $("#insertResult").text("Your language is C#!")
+    else if(tally > 5 && tally <= 10){
+      $("#insertResult").append("Your language is C#!")
     }
-    else if(tally >= 16 && tally <= 20){
-      $("#insertResult").text("Your language is Java!")
+    else if(tally > 11 && tally <= 15){
+      $("#insertResult").append("Your language is Java!")
+    }
+    else if(tally > 15 && tally <=20){
+      $("#insertResult").append("Your language is Javascript!")
     }
     else{
-      $("#insertResult").text("Your language is unknown. Sorry! Something went wrong.")
+      $("#insertResult").append("Your language is unknown. Sorry! Something went wrong")
     }
     $("#submitButton").hide();
     $("#questionFive").hide()
     $("#result").show();
-    $("#restartButton").show()
     event.preventDefault();
   });
 });
